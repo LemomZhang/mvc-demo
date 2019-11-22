eb1ab2832f25a2f0ec9e8827403331f652f1205f
zlm <lemom211@outlook.com>
2019-11-23 02:06:09 +0800

    ----build------

--- src/app2.js
+++ src/app2.js
@@ -3,12 +3,14 @@ import $ from 'jquery'
 
 const $tabBar = $('#app2 .tab-bar')
 const $tabContent = $('#app2 .tab-content')
+const index = localStorage.getItem('index') || 0
 
 $tabBar.on('click','li',(e)=>{
     let $li = $(e.currentTarget)
     $li.addClass('selected').siblings().removeClass('selected')
     let $index = $li.index()
+    localStorage.setItem('index',$index)
     $tabContent.children().eq($index).addClass('active').siblings().removeClass('active')
 })
 
-$tabBar.children().eq(0).trigger('click')
+$tabBar.children().eq(index).trigger('click')
